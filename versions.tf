1fae71a207ed7dd9cea73963326ea62960a95ad5
Michael Kam <michaelkam@192-168-1-101.tpgi.com.au>
2022-08-04 10:26:46 +1000

    Remove cloud block

--- versions.tf
+++ versions.tf
@@ -1,12 +1,12 @@
 terraform {
 
-  cloud {
-    organization = "hashicorp-learn"
+  # cloud {
+  #   organization = "michaelk-training"
 
-    workspaces {
-      name = "learn-terraform-cloud"
-    }
-  }
+  #   workspaces {
+  #     name = "learn-terraform-cloud"
+  #   }
+  # }
 
   required_providers {
     aws = {
